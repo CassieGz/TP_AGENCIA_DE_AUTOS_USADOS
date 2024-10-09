6fbd4c617fe83f1dc7765456b225a353bebf6b1d
Correa <ccorrea@FRSN.UTN.EDU.AR>
2024-10-08 21:27:48 -0300

    Agregado de clases

---
+++ TP_AGENCIA_DE_AUTOS/Camion.cs
@@ -0,0 +1,45 @@
+﻿using SistemaGestion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_AGENCIA_DE_AUTOS
+{
+    internal class Camion : Vehiculo
+    {
+        //
+        private bool caja_carga;
+        private string dimension_caja;
+        private int carga_max;
+
+
+        //
+
+
+        //
+        public override void Marca()
+        {
+            //
+        }
+
+        //
+        public bool Caja_carga
+        {
+            get { return this.caja_carga; }
+            set { this.caja_carga = value; }
+        }
+        public string Dimension_caja
+        {
+            get { return dimension_caja; }
+            set { dimension_caja = value; }
+        }
+        public int Carga_max
+        {
+            get { return this.carga_max; }
+            set { this.carga_max = value; }
+        }
+
+    }
+}
---
+++ TP_AGENCIA_DE_AUTOS/Moto.cs
@@ -0,0 +1,36 @@
+﻿
+using SistemaGestion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_AGENCIA_DE_AUTOS
+    {
+        internal class Moto : Vehiculo
+        {
+            //prop priv
+            private string cilindrada;
+
+
+        //constr
+        public Moto()
+        {
+
+        }
+
+            //metod
+            public override void Marca()
+            {
+                //
+            }
+
+            //public
+            public string Cilindrada
+            {
+                get { return this.cilindrada; }
+                set { this.cilindrada = value; }
+            }
+        }
+    }
