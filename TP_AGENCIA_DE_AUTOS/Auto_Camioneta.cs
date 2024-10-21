eccb654ae141d6c602ef82dc9bf7af664361a9ff
daiana <sayanagodoy22@gmail.com>
2024-10-21 18:50:47 -0300

    arreglo en auto_camioneta

--- TP_AGENCIA_DE_AUTOS/Auto_Camioneta.cs
+++ TP_AGENCIA_DE_AUTOS/Auto_Camioneta.cs
@@ -1,31 +1,17 @@
-<<<<<<< HEAD
-﻿using system;
-using system.collections.generic;
-using system.linq;
-using system.text;
-using system.threading.tasks;
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
->>>>>>> ca331a8709fdfe476af235f46661fabd906fdd11
 
-namespace sistemagestion
+namespace SistemaGestion
 {
-    internal class auto_camioneta : vehiculo
+    internal class Auto_Camioneta : Vehiculo
     {
-<<<<<<< HEAD
-
-        public override void marca()
-=======
-        //
-        //
+        
         public Auto_Camioneta(int id_vehiculo, string patente, int kilometro, short anio, int id_marca, string modelo, int id_segmento, int id_combustible, float precio_vta, bool t_observaciones, string observaciones) : base(id_vehiculo, patente, kilometro, anio, id_marca, modelo, id_segmento, id_combustible, precio_vta, t_observaciones, observaciones)
->>>>>>> ca331a8709fdfe476af235f46661fabd906fdd11
-        {
+       {
 
         }
 
