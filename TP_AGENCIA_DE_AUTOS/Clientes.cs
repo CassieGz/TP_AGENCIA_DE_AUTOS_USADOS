689cdd38116b9c4d908f166d5c87675e832aadda
daiana <sayanagodoy22@gmail.com>
2024-10-25 18:57:51 -0300

    agrege metodos de clientes y ventas

---
+++ TP_AGENCIA_DE_AUTOS/Clientes.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_AGENCIA_DE_AUTOS
+{
+    internal class Clientes
+    {
+        private int IdCli;
+        private string Cli;
+        private long Cuit;
+        private string Domicilio;
+        private int IdLocalidad;
+        private long Telefonos;
+        private string Correo;
+
+        public Clientes()
+        {
+
+        }
+        public Clientes(int idclientes, string cliente, long cuit, string domicilio,
+                        int idlocalidad, long telefonos, string correo)
+        {
+            this.IdCli = idclientes;
+            this.Cli = cliente;
+            this.cuit = cuit;
+            this.Domicilio = domicilio;
+            this.idlocalidad = idlocalidad;
+            this.Telefonos = telefonos;
+            this.Correo = correo;
+        }
+        public int idcli
+        {
+            get { return this.idcli; }
+            set { this.idcli = value; }
+        }
+        public string cliente
+        {
+            get { return this.cliente; }
+            set { this.cliente = value; }
+        }
+        public long cuit
+        {
+            get { return this.cuit; }
+            set { this.cuit = value; }
+        }
+        public string domicilio
+        {
+            get { return this.domicilio; }
+            set { this.domicilio = value; }
+        }
+        public int idlocalidad
+        {
+            get { return this.idlocalidad; }
+            set { this.idlocalidad = value; }
+        }
+        public long telefonos
+        {
+            get { return this.telefonos; }
+            set { this.telefonos = value; }
+        }
+        public string correo
+        {
+            get { return this.correo; }
+            set { this.correo = value; }
+        }
+        private void Mostrarclientes()
+        {
+            Console.WriteLine("|===========================================================|");
+
+            Console.WriteLine("|ID   | Cliente     |  CUIT     | Localidad     | Telefono  |");
+
+            Console.WriteLine("|===========================================================|");
+
+            Console.WriteLine("| Domicilio    | Correo            |                        |");
+
+            Console.WriteLine("|===========================================================|");
+
+            Console.WriteLine("\n");
+        }
+        private void Agregarclientes()
+        {
+            Console.WriteLine("Agregar un cliente");
+            Console.WriteLine("Ingrese Id:");
+            int idcliente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese cliente:");
+            string cliente = Console.ReadLine();
+            Console.WriteLine("Ingrese cuit:");
+            long cuit = long.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese domicilio:");
+            string domicilio = Console.ReadLine();
+            Console.WriteLine("Ingrese localidad:");
+            int idlocalidad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese telefono:");
+            long telefono = long.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese correo:");
+            string correo = Console.ReadLine();
+        }
+        private void Eliminarclientes()
+        {
+            Clientes cliente = listaClientes.Find(c => c.idClientes == id);
+            if (cliente != null)
+            {
+                listaClientes.Remove(cliente);
+                Console.WriteLine("Cliente eliminado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Cliente no encontrado.");
+            }
+            Console.ReadKey();
+        }
+
+        private void Actualizarclientes()
+        {
+            Clientes clientes = listaClientes.Find(c => c.idClientes == id);
+            if (cliente != null)
+            {
+                Console.Write("Nuevo nombre del cliente: ");
+                string Clientes = Console.ReadLine();
+                Console.Write("Nuevo CUIT: ");
+                long Cuit = long.Parse(Console.ReadLine());
+                Console.Write("Nuevo domicilio: ");
+                string Domicilio = Console.ReadLine();
+                Console.Write("Nuevo ID Localidad: ");
+                int idlocalidad = int.Parse(Console.ReadLine());
+                Console.Write("Nuevo teléfono: ");
+                long Telefonos = long.Parse(Console.ReadLine());
+                Console.Write("Nuevo correo: ");
+                string Correo = Console.ReadLine();
+                Console.WriteLine("Cliente actualizado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Cliente no encontrado.");
+            }
+        }
+        public void listarclientes()
+        { 
+
+        }
+
+    }
+}
---
+++ TP_AGENCIA_DE_AUTOS/Ventas.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_AGENCIA_DE_AUTOS
+{
+    internal class Ventas
+    {
+        private int IdCli;
+        private int IdVeh;
+        private DateTime FecCompra;
+        private DateTime FecEntrega;
+        private double SubT;
+        private double Iva;
+        private double Desc;
+        private double Total;
+
+        public Ventas()
+        {
+        }
+        public Ventas(int idcliente, int idvehiculo, DateTime fechacompra, DateTime fechaentrega,
+                      double subtotal, double iva, double descuento, double total)
+        {
+            this.IdCli = idcliente;
+            this.IdVeh = idvehiculo;
+            this.FecCompra = fechacompra;
+            this.FecEntrega = fechaentrega;
+            this.SubT = subtotal;
+            this.Iva = iva;
+            this.Desc = descuento;
+            this.Total = total;
+        }
+        public int IdClientes
+        {
+            get { return this.IdCli; }
+            set { this.IdCli = value; }
+        }
+        public int IdVehiculo
+        {
+            get { return this.IdVeh; }
+            set { this.IdVeh = value; }
+        }
+        public DateTime FechaCompra
+        {
+            get { return this.FechaCompra; }
+            set { this.FechaCompra = value; }
+        }
+        public DateTime FechaEntrega
+        {
+            get { return this.FechaEntrega; }
+            set { this.FechaEntrega = value; }
+        }
+        public double SubTotal
+        {
+            get { return this.SubTotal; }
+            set { this.SubTotal = value; }
+        }
+        public double iva
+        {
+            get { return this.iva; }
+            set { this.iva = value; }
+        }
+        public double Descuento
+        {
+            get { return this.Desc; }
+            set { this.Desc = value; }
+        }
+        public double total
+        {
+            get { return this.total; }
+            set { this.total = value; }
+        }
+        private void Mostrarventas()
+        {
+            Console.WriteLine("|======================================================================|");
+
+            Console.WriteLine("|  ID  |   Cliente   |   Vehiculo   |  Fecha Compra  |  Fecha Entrega  |");
+
+            Console.WriteLine("|======================================================================|");
+
+            Console.WriteLine("|     Subtotal     |    IVA    |     Descuento     |       Total       |");
+
+            Console.WriteLine("|======================================================================|");
+
+            Console.WriteLine("\n");
+        }
+        private void Agregarventas()
+        {
+            Console.WriteLine("Agregar una venta");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Ingrese Id de cliente");
+            int idcliente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar vehiculo");
+            int idvehiculo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar fecha de compra");
+            DateTime fechacompra = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar fecha de entrega");
+            DateTime fechaentrega = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar subtotal");
+            double subtotal = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar IVA");
+            double iva = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar descuento");
+            double descuento = Double.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingresar total");
+            //double total = Double.Parse(Console.ReadLine());
+            double total = subtotal + iva - descuento;
+
+            Ventas nuevaVenta = new Ventas(idcliente, idvehiculo, fechacompra, fechaentrega, subtotal, iva,
+                                           descuento, total);
+            //listaVentas.Add(nuevaVenta);
+
+            Console.WriteLine("¡Venta agregada con éxito!");
+        }
+
+
+        private void EliminarVentas()
+        {
+            Ventas venta = listaVentas.Find(v => v.IdCli == idcliente);
+            if (venta != null)
+            {
+                listaVentas.Remove(venta);
+                Console.WriteLine("¡Venta eliminada con éxito!");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró una venta con ese ID de cliente.");
+            }
+        }
+        private void Actualizarventas()
+        {
+            Ventas ventas = listaVentas.Find(v => v.IdCli == idcliente);
+            if (ventas != null)
+            {
+                Console.WriteLine("Actualizar los datos de la venta:");
+                Console.Write("Ingresar nueva fecha de entrega (dd/MM/yyyy): ");
+                DateTime nuevaFechaEntrega = DateTime.Parse(Console.ReadLine());
+                Console.Write("Ingresar nuevo subtotal: ");
+                double nuevoSubtotal = double.Parse(Console.ReadLine());
+                Console.Write("Ingresar nuevo IVA: ");
+                double nuevoIVA = double.Parse(Console.ReadLine());
+                Console.Write("Ingresar nuevo descuento: ");
+                double nuevoDescuento = double.Parse(Console.ReadLine());
+                double nuevoTotal = nuevoSubtotal + nuevoIVA - nuevoDescuento;
+
+                ventas.FecEntrega = nuevaFechaEntrega;
+                ventas.SubT = nuevoSubtotal;
+                ventas.Iva = nuevoIVA;
+                ventas.Desc = nuevoDescuento;
+                ventas.Total = nuevoTotal;
+
+                Console.WriteLine("¡Venta actualizada con éxito!");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró una venta con ese ID de cliente.");
+            }
+
+        }
+        public void listarventas()
+        {
+            
+        }
+
+
+    }
+}
